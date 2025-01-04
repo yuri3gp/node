a848f1273964e37f9d4c533a77d4cee5f07a2335
yuri3gp <yuri3gp@gmail.com>
2025-01-03 21:25:37 -0300

    postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,26 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { User } from './users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'root',
-    password: 'root',
-    database: 'banco',
-    entities: [User],
-    synchronize: true,
-    autoLoadEntities: false
-  })],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: false,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
