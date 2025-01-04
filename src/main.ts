a513ec87a1ea64463ca7f824edf5584d5a422ee8
yuri3gp <yuri3gp@gmail.com>
2025-01-03 22:10:47 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://react-mrbk.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
